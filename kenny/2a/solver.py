c99a8fb2d7bb8e457b127262afe448d6fb15815a
Kenny Cheung <kcomain@protonmail.com>
2020-12-02 15:28:18 +0800

    newday broke lmfao

--- kenny/2a/solver.py
+++ kenny/2a/solver.py
@@ -1,6 +1,32 @@
+
 import sys
 
 sys.path.extend(['../'])
 import utils
 
-utils.info('Solver for day 2a')
+utils.info('Solver for day 1a')
+
+helptext = f'''Usage: python3 newday.py [file]
+
+Required parameters:
+    file: input file
+'''
+
+
+def _help():
+    print(helptext)
+
+if len(sys.argv) == 1:
+    utils.warn('no input file specified.')
+    _help()
+    sys.exit(0)
+
+try:
+    with open(sys.argv[1], 'r') as file:
+        utils.debug('reading file')
+        puzzle = file.read()
+except FileNotFoundError:
+    utils.error(f'file "{sys.argv[1]}" does not exist.')
+    _help()
+    sys.exit(1)
+
--- kenny/newday.py
+++ kenny/newday.py
@@ -19,12 +19,38 @@ print('no')
 os.mkdir(dirname)
 utils.debug(f'created directory {dirname}')
 
-template = f"""import sys
+template = f"""
+import sys
 
 sys.path.extend(['../'])
 import utils
 
-utils.info('Solver for day {dirname}')
+utils.info('Solver for day 1a')
+
+helptext = f'''Usage: python3 {sys.argv[0]} [file]
+
+Required parameters:
+    file: input file
+'''
+
+
+def _help():
+    print(helptext)
+
+if len(sys.argv) == 1:
+    utils.warn('no input file specified.')
+    _help()
+    sys.exit(0)
+
+try:
+    with open(sys.argv[1], 'r') as file:
+        utils.debug('reading file')
+        puzzle = file.read()
+except FileNotFoundError:
+    utils.error(f'file "{{sys.argv[1]}}" does not exist.')
+    _help()
+    sys.exit(1)
+
 """
 utils.debug("templating solver.py")
 file = open(os.path.join(dirname, 'solver.py'), 'w+')
