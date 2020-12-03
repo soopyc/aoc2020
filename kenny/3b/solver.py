b4d31276b42663e4b31462316bede02437dbce54
Kenny Cheung <kcomain@protonmail.com>
2020-12-03 16:17:55 +0800

    day 3 part b done

--- kenny/3b/solver.py
+++ kenny/3b/solver.py
@@ -1,23 +1,25 @@
 
 import sys
+import json
 
 sys.path.extend(['../'])
 import utils
 
 utils.info('Solver for day 3b')
 
-helptext = f'''Usage: python3 {sys.argv[0]} [file]
+helptext = f'''Usage: python3 {sys.argv[0]} [file] [ruleset]
 
 Required parameters:
     file: input file
+ ruleset: ruleset json file
 '''
 
 
 def _help():
     print(helptext)
 
-if len(sys.argv) == 1:
-    utils.warn('no input file specified.')
+if len(sys.argv) != 3:
+    utils.warn('no input file/too many argument specified.')
     _help()
     sys.exit(0)
 
@@ -29,4 +31,64 @@ except FileNotFoundError:
     utils.error(f'file "{sys.argv[1]}" does not exist.')
     _help()
     sys.exit(1)
+
+try:
+    with open(sys.argv[2], 'r') as file:
+        utils.debug('reading file')
+        ruleset = json.load(file)
+except FileNotFoundError:
+    utils.error(f'file "{sys.argv[1]}" does not exist.')
+    _help()
+    sys.exit(1)
+
+utils.debug('setting up map')
+_map = puzzle.splitlines()
+
+def run(mx, my):
+    utils.debug('run called')
+    x, y = 0, 0
+    trees = 0
+    ok = 0
+    c = 0
+    
+    try:
+        while True:
+            if c == 0:
+                c += 1
+                continue
+            c += 1
+            x += mx
+            y += my
+            level = _map[y]
+            location = x % len(level)
+            if level[location] == "#":
+                #tree = level[location]
+                utils.warn(level, end='\n', text="TREE")
+                trees += 1
+            else:
+                utils.success(level, end='\n', text=" OK ")
+                ok += 1
+    except IndexError:
+        pass
+    return trees, ok
+
+
+utils.debug('start iterating rulesets')
+ttrees = 0
+tok = 0
+ttrees_arr = []
+for i in ruleset['']:
+    _t, _o = run(i[0], i[1])
+    ttrees_arr.append(_t if _t != 0 else 1)
+    ttrees += _t
+    tok += _o
+
+res = 1
+utils.debug(ttrees_arr)
+for i in ttrees_arr:
+    res *= i
+
+print()
+utils.info(f'Ran map \n\tTrees: {ttrees}\n\tOkays: {tok}\n\tPart2: {res}',
+           end='\n')
 
