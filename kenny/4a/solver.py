c61e15f1d6126ea1a6acb28b104d4e619da690ef
Kenny Cheung <kcomain@protonmail.com>
2020-12-04 13:38:00 +0800

    day 4 part 1

--- kenny/4a/solver.py
+++ kenny/4a/solver.py
@@ -30,3 +30,29 @@ except FileNotFoundError:
     _help()
     sys.exit(1)
 
+required_fields = 'byr iyr eyr hgt hcl ecl pid'.split()
+
+#required_dict = {}
+#for i in required_fields:
+#    required_dict[i] = 0
+
+utils.debug('prepping passport data')
+passports = puzzle.split('\n\n')
+successes = 0
+fails = 0
+for c, i in enumerate(passports):
+    #cpass = required_dict.copy()
+    passq = True
+    passpt = i.replace('\n', ' ')
+    for field in required_fields:
+        if f'{field:}' not in i:
+            utils.error(f'Passport #{c} doesn\'t pass check', text='FAIL')
+            passq = False
+            fails += 1
+            break
+    if passq:
+        successes += 1
+        utils.success(f'Passport #{c} pass checks', text='PASS')
+
+utils.info(f'pass: {successes}\nfail: {fails}')
+
