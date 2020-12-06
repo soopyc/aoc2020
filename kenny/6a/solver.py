6f7538c033aed04b3599e6823fba0c9476880821
Kenny Cheung <kcomain@protonmail.com>
2020-12-06 23:03:12 +0800

    day 6 part 1

---
+++ kenny/6a/solver.py
@@ -0,0 +1,73 @@
+
+import sys
+
+sys.path.extend(['../'])
+import utils
+
+utils.info('Solver for day 6a')
+
+helptext = f'''Usage: python3 {sys.argv[0]} [file]
+
+Required parameters:
+    file: input file
+'''
+
+
+def _help():
+    print(helptext)
+
+if len(sys.argv) == 1:
+    utils.warn('no input file specified.')
+    _help()
+    sys.exit(0)
+
+try:
+    with open(sys.argv[1], 'r') as file:
+        utils.debug('reading file')
+        puzzle = file.read()
+except FileNotFoundError:
+    utils.error(f'file "{sys.argv[1]}" does not exist.')
+    _help()
+    sys.exit(1)
+
+
+class Person:
+    __slots__ = ('answers', )
+    def __init__(self, ans):
+        self.answers = []
+        for i in ans:
+            self.answers.append(i) if i not in self.answers else 0
+        #print(self.answers)
+
+    def __len__(self):
+        return len(self.answers)
+
+
+class Group:
+    def __init__(self, answers):
+        self.answers = answers
+        self.people = []
+        self.parse()
+    
+    def parse(self):
+        raw_ans = self.answers.strip()
+        self.answers = []
+
+        for i in raw_ans.splitlines():
+            person = Person(i)
+            self.people.append(person)
+            for a in person.answers:
+                self.answers.append(a) if a not in self.answers else 0
+
+    def __len__(self):
+        return len(self.answers)
+
+count = 0
+for i in puzzle.strip().split('\n\n'):
+    gp = Group(i)
+    utils.debug(f'Group answers: {len(gp)}')
+    count += len(gp)
+
+
+utils.info(f'total answers: {count}')
+
